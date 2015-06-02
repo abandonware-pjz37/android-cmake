5513e024328e856ba63144c38b2635bdf8e27836
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-06-02 20:02:29 +0200

    Remove unused

--- common.cmake
+++ common.cmake
@@ -3,7 +3,7 @@
 
 # * https://github.com/hunter-packages/gate
 # * https://github.com/ruslo/hunter
-include("cmake/HunterGate.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/cmake/HunterGate.cmake")
 HunterGate(
     URL "https://github.com/ruslo/hunter/archive/android-005.tar.gz"
     SHA1 "3a18ecc61cad989aef50abafdaa88c72291eabc5"

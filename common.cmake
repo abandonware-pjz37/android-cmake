6bd6ff5599385fbf2700ede5be462079fa14ec3e
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-06-02 19:51:42 +0200

    HunterGate: DRY use

---
+++ common.cmake
@@ -0,0 +1,10 @@
+# Copyright (c) 2015, Ruslan Baratov
+# All rights reserved.
+
+# * https://github.com/hunter-packages/gate
+# * https://github.com/ruslo/hunter
+include("cmake/HunterGate.cmake")
+HunterGate(
+    URL "https://github.com/ruslo/hunter/archive/android-004.tar.gz"
+    SHA1 "a3c46a481bd8583c600f819366ed0d0f81b9b3d1"
+)

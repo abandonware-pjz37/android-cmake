46bb094cc161405d750a36bc8d2fa201decac4e3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-06-16 21:20:31 +0200

    Allow custom config location

--- common.cmake
+++ common.cmake
@@ -7,4 +7,5 @@ include("${CMAKE_CURRENT_LIST_DIR}/cmake/HunterGate.cmake")
 HunterGate(
     URL "https://github.com/ruslo/hunter/archive/android-005.tar.gz"
     SHA1 "a26088094228c90f748c41e003c6496a858a5f17"
+    ${CONFIG_FILE}
 )

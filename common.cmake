df62949a3891194a095c380c46db51482dbaaf87
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-06-17 15:31:04 +0200

    Update Hunter

--- common.cmake
+++ common.cmake
@@ -5,7 +5,7 @@
 # * https://github.com/ruslo/hunter
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/HunterGate.cmake")
 HunterGate(
-    URL "https://github.com/ruslo/hunter/archive/android-005.tar.gz"
-    SHA1 "a26088094228c90f748c41e003c6496a858a5f17"
+    URL "https://github.com/ruslo/hunter/archive/v0.11.1.tar.gz"
+    SHA1 "3b8ffc911b3cd8308547ef1d3a5a69331dab5c19"
     ${CONFIG_FILE}
 )

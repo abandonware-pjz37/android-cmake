8fcd50a7fb947e0bd5bf7729f647ca8de7423f10
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-07-27 05:43:29 +0300

    Remove unused

--- 05-qt-hellogl2/static_qt_plugins.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2015, Ruslan Baratov
-// All rights reserved.
-
-#include <QtPlugin>
-
-// http://doc.qt.io/qt-5/qtglobal.html
-
-Q_IMPORT_PLUGIN(QAndroidPlatformIntegrationPlugin);

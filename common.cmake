fb2a831f5122b8dbfbb66cee9d6f69ce0a53fc83
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-09-18 23:30:29 +0300

    Fix misprint

--- common.cmake
+++ common.cmake
@@ -5,7 +5,7 @@
 # * https://github.com/ruslo/hunter
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/HunterGate.cmake")
 HunterGate(
-    ULR "https://github.com/ruslo/hunter/archive/v0.12.11.tar.gz"
+    URL "https://github.com/ruslo/hunter/archive/v0.12.11.tar.gz"
     SHA1 "79173425791e1fac4bafdecfc47a4edb994b2b1d"
     ${CONFIG_FILE}
 )

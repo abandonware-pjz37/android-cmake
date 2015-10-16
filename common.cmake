88b31ad6c861a87831e86943b1dbfb261bc29415
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-10-16 03:17:52 +0300

    Update Hunter

--- common.cmake
+++ common.cmake
@@ -5,7 +5,7 @@
 # * https://github.com/ruslo/hunter
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/HunterGate.cmake")
 HunterGate(
-    URL "https://github.com/ruslo/hunter/archive/v0.12.11.tar.gz"
-    SHA1 "79173425791e1fac4bafdecfc47a4edb994b2b1d"
+    URL "https://github.com/ruslo/hunter/archive/v0.12.22.tar.gz"
+    SHA1 "868408f1f3d8deeaab13f4ebca28b02ec2dfcade"
     ${CONFIG_FILE}
 )

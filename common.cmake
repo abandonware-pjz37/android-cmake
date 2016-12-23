ff2843d8db961206ca81f85ac245385e613ba935
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-12-23 23:51:45 +0800

    Update Hunter

--- common.cmake
+++ common.cmake
@@ -1,11 +1,11 @@
-# Copyright (c) 2015, Ruslan Baratov
+# Copyright (c) 2015-2016, Ruslan Baratov
 # All rights reserved.
 
 # * https://github.com/hunter-packages/gate
 # * https://github.com/ruslo/hunter
 include("${CMAKE_CURRENT_LIST_DIR}/cmake/HunterGate.cmake")
+
 HunterGate(
-    URL "https://github.com/ruslo/hunter/archive/v0.15.6.tar.gz"
-    SHA1 "3dc0cd2ccbc52e8be93967dbc583303a1e825aa2"
-    ${CONFIG_FILE}
+    URL "https://github.com/ruslo/hunter/archive/v0.17.7.tar.gz"
+    SHA1 "78219f42c3737adcc161c52b9f71ce65a3d775c2"
 )
